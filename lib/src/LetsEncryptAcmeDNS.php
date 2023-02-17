928b3edd9a5ac18181b22f87fcbf325255f3fd3d
takuya <55338+takuya@users.noreply.github.com>
2023-02-17 18:04:24 +0900

    change public method

--- lib/src/LetsEncryptAcmeDNS.php
+++ lib/src/LetsEncryptAcmeDNS.php
@@ -44,34 +44,41 @@ class LetsEncryptAcmeDNS {
     return $domain_names;
   }
   
-  public function orderNewCert ( $acme_uri = LetsEncryptServer::STAGING, callable $on_dns_wait=null ): CertificateWithPrivateKey {
+  protected function newOrder ( AsymmetricKey $domain_pkey,
+                                string        $acme_uri,
+                                callable      $on_dns_wait = null ): CertificateWithPrivateKey {
     // keys
-    $owner_pky = new AsymmetricKey( $this->priv_key );
-    // cert keys
-    $domain_key = new AsymmetricKey();
+    $owner_pkey = new AsymmetricKey( $this->owner_priv_key );
     $dn = new CSRSubject( ...['commonName' => $this->domain_names[0], 'subjectAlternativeNames' => $this->domain_names] );
     
     // start lets encrypt ACMEv2 process
-    $cli = new AcmePHPWrapper( $owner_pky->privKey(), $acme_uri );
+    $cli = new AcmePHPWrapper( $owner_pkey->privKey(), $acme_uri );
     //
     $cli->newAccount( $this->owner_email );
     $cli->newOrder( $this->domain_names );
     $challenges = $cli->getDnsChallenge();
-    $on_wait = $on_dns_wait ??function( $name, $type, $content ) {
-      $message = sprintf( '...wait ( %s, %s, %s...) for update TXT in SOA NS.'.PHP_EOL ,
-          $name, $type, substr($content,'0',5));
-      $this->log($message);
+    $on_wait = $on_dns_wait ?? function( $name, $type, $content ) {
+      $message = sprintf( '...wait ( %s, %s, %s...) for update TXT in SOA NS.'.PHP_EOL,
+        $name, $type, substr( $content, '0', 5 ) );
+      $this->log( $message );
     };
-    $this->processDNSTask($challenges,$on_wait);
+    $this->processDNSTask( $challenges, $on_wait );
     
     // Finalize order.
-    $cli->finalizeOrderCertificate( $this->domain_names[0], $dn, $domain_key->privKey() );
+    $cli->finalizeOrderCertificate( $this->domain_names[0], $dn, $domain_pkey->privKey() );
     //// Get Result.
     $ret = $cli->certificateLastIssued();
-    $cert_and_a_key = new CertificateWithPrivateKey( $domain_key->privKey(), $ret['cert'], $ret['intermediate'] );
+    $cert_and_a_key = new CertificateWithPrivateKey( $domain_pkey->privKey(), $ret['cert'], $ret['intermediate'] );
     return $cert_and_a_key;
   }
-  protected function processDNSTask($challenges,$on_wait){
+  
+  public function orderNewCert ( string   $domain_pkey_pem= null , $acme_uri = LetsEncryptACMEServer::STAGING,
+                                 callable $on_dns_wait = null ): CertificateWithPrivateKey {
+    $domain_key = $domain_pkey_pem ? new AsymmetricKey($domain_pkey_pem): new AsymmetricKey();
+    return $this->newOrder( $domain_key, $acme_uri, $on_dns_wait );
+  }
+  
+  protected function processDNSTask ( $challenges, $on_wait ) {
     /** @var \Fiber[] $fibers */
     $fibers = [];
     foreach (  $challenges as $key => $challenge) {
