0e78f8fac598ebfbf3a06022aa0e0e55eeedf63f
takuya <55338+takuya@users.noreply.github.com>
2026-05-12 21:07:33 +0900

    resolverを指定

--- tests/Units/DNSResolveTest.php
+++ tests/Units/DNSResolveTest.php
@@ -3,12 +3,29 @@
 namespace tests\Units;
 use tests\TestCase;
 use function Takuya\Utils\dns_resolve;
-use function Takuya\Utils\domain_ns;
 use tests\assertions\AssertIPAddress;
+use PHPUnit\Framework\Exception;
 
 class DNSResolveTest extends TestCase {
   use AssertIPAddress;
   
+  /**
+   * @throws \Exception dns resolve Exception
+   */
+  public function test_resolve_via_resolver() {
+    $resolvers=['8.8.8.8',/*'8.8.4.4'*/];
+    foreach ($resolvers as $resolver){
+      try {
+        $ret = dns_resolve('.', 'ns',$resolver);
+        $this->assertNotEmpty($ret);
+        $this->assertIsString($ret);
+        $this->assertStringContainsString('root-servers.net',$ret);
+      }catch (\Exception $e){
+        throw new Exception("udp/53 failed. Check connectivity to {$resolver}");
+      }
+    }
+  }
+
   public function test_resolve_dns_NS(){
     $ret = dns_resolve('.', 'ns');
     $this->assertNotEmpty($ret);
