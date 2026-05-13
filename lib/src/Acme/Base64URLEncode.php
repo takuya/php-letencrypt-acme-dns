1f5accd3803d714435d22a729c5c163297db39eb
takuya <55338+takuya@users.noreply.github.com>
2026-05-13 15:02:04 +0900

    Base64をクラスにした

---
+++ lib/src/Acme/Base64URLEncode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Takuya\LEClientDNS01\Acme;
+
+class Base64URLEncode {
+  public static function encode(string $string): string {
+    return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
+  }
+  
+  public static function decode(string $str): string  {
+    if( $len = strlen($str) % 4 > 0){
+      $str = str_pad($str,$len+($len%4>0?4-$len%4:0),'=');
+    }
+    return base64_decode(strtr($str, '-_', '+/'));
+  }
+}
