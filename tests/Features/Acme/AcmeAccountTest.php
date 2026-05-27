8fad7f1f2a577b5fd93e660a8806bd64c5ec380c
takuya <55338+takuya@users.noreply.github.com>
2026-05-27 13:43:14 +0900

    テスト

--- tests/Features/Acme/AcmeAccountTest.php
+++ tests/Features/Acme/AcmeAccountTest.php
@@ -1,28 +1,52 @@
 <?php
 
-namespace tests\Units\Acme;
+namespace tests\Features\Acme;
 
-use tests\TestCase;
 use Takuya\LEClientDNS01\Acme\Resources\AcmeDirectory;
-use Takuya\LEClientDNS01\Acme\Resources\Directory\AcmeEndpointEnum;
+use Takuya\LEClientDNS01\Acme\Resources\AcmeDirectoryEnum;
 use Takuya\LEClientDNS01\PKey\AsymmetricKey;
 use Takuya\RandomString\RandomString;
 use Takuya\LEClientDNS01\Acme\AcmeAccount;
 use Takuya\LEClientDNS01\Acme\AcmeClient;
+use Takuya\LEClientDNS01\LetsEncryptACMEServer;
+use Takuya\LEClientDNS01\Acme\Http\AcmeHttpException;
 
-class AcmeAccountTest extends TestCase {
+class AcmeAccountTest extends AcmeTestCase {
   public function test_create_acme_account() {
+    //
+    try {
+      $str = RandomString::gen( 5, RandomString::LOWER );
+      $domain_name = "phpunit-{$str}.{$this->base_domain}";
+      $pkey = new AsymmetricKey();
+      $email = sprintf( "admin-%s@%s", RandomString::gen( 10, RandomString::LOWER ), $domain_name );
+      
+      //
+      $dir = new AcmeDirectory( LetsEncryptACMEServer::STAGING );
+      $dir->getDirectoryUrl( AcmeDirectoryEnum::newNonce );
+      $cli = new AcmeClient( $dir );
+      $nonce = $cli->newNonce();
+      $account = new AcmeAccount( $email, $pkey->privKey() );
+      $cli->newAccount( $account, $nonce );
+      $this->expectNotToPerformAssertions();
+    }catch (\Exception $e){
+      throw $e;
+    }
+  }
+  
+  public function test_create_invalid_acme_account() {
+    $this->expectException( AcmeHttpException::class );
+    //
+    $str = RandomString::gen( 5, RandomString::LOWER );
+    $domain_name = "phpunit-{$str}.{$this->base_domain}";
     $pkey = new AsymmetricKey();
-    $email = sprintf("admin-%s@example.tld", RandomString::gen(10,RandomString::LOWER));
-    $account = new AcmeAccount($email, $pkey->privKey());
-    $STAGING = 'https://acme-staging-v02.api.letsencrypt.org/directory';
-
+    $email = sprintf( "admin-%s@%s.example.tld", RandomString::gen( 10, RandomString::LOWER ), $domain_name );
     
-    $dir = new AcmeDirectory($STAGING);
-    $dir->getDirectoryUrl(AcmeEndpointEnum::newNonce);
-    $cli = new AcmeClient($dir);
+    //
+    $dir = new AcmeDirectory( LetsEncryptACMEServer::STAGING );
+    $dir->getDirectoryUrl( AcmeDirectoryEnum::newNonce );
+    $cli = new AcmeClient( $dir );
     $nonce = $cli->newNonce();
-    $ret = $cli->newAccount($account,$nonce);
-    dump($ret);
+    $account = new AcmeAccount( $email, $pkey->privKey() );
+    $cli->newAccount( $account, $nonce );
   }
 }
